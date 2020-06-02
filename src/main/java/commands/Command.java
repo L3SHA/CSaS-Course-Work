4b0f650b08c6d95e6eb6a5a8aefd229a3a0445e6
L3SHA <lesha18.02.02001@gmail.com>
2020-06-02 22:54:20 +0300

    Introduce classes for bot commands

---
+++ src/main/java/commands/Command.java
@@ -0,0 +1,32 @@
+package commands;
+
+import locale.LocaleService;
+import locale.LocaleServiceImpl;
+import responses.Response;
+import user.User;
+
+public abstract class Command  {
+
+    protected static LocaleService localeService = LocaleServiceImpl.getInstance();
+    protected static CommandEnum commandName;
+    protected Response response;
+    protected String s;
+    protected User user;
+    public Command(String s, User user) {
+        this.s = s;
+        this.user = user;
+    }
+
+    public CommandEnum getCommandName() {
+        return commandName;
+    }
+
+    public void setCommandName(CommandEnum commandName) {
+        this.commandName = commandName;
+    }
+
+    public abstract Response execute();
+
+}
+
+
---
+++ src/main/java/commands/CommandEnum.java
@@ -0,0 +1,52 @@
+package commands;
+
+import locale.LocaleService;
+import locale.LocaleServiceImpl;
+
+import java.util.Locale;
+
+public enum CommandEnum {
+    MENU("/menu", "menu_item"),
+    AUTH("/auth", "auth_item"),
+    LOGIN("/login", "login_item"),
+    LOGOUT("/logout", "logout_item"),
+    PROFILE("/profile", "profile_item"),
+    PROFILE_INFO("/profileinfo", "profile_info_item"),
+    RESUMES("/resumes", "resumes_item"),
+    CREATE_RESUME("/createresume", "create_resume_item"),
+    SEARCH("/search", "search_item"),
+    FIND("/find", "find_item"),
+    BACK_MENU("/backmenu", "back_menu_item"),
+    FAVORITES("/favorites", "favorites_item"),
+    CATALOGUES("/catalogues", "catalogues_item"),
+    SALARYFROM("/salaryfrom", "salary_from_item"),
+    SALARYTO("salaryto", "salary_to_item"),
+    AGE("/age", "age_item"),
+    EXPERIENCE("/experience", "experience_item"),
+    PLACEOFWORK("/placeofwork", "place_of_work_item"),
+    LANGUAGE("/lang", "language_item"),
+    PREVIOUSPAGE("/previouspage", "previous_page_item"),
+    NEXTPAGE("/nextpage", "next_page_item"),
+    OTHER("undefined", "undefined_item");
+
+
+    private static LocaleService localeService = LocaleServiceImpl.getInstance();
+    private String command;
+    private String name;
+
+
+    CommandEnum(String command, String name) {
+        this.command = command;
+        this.name = name;
+    }
+
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getCaption(Locale locale) {
+        return localeService.getMenuItemBundle(locale).getString(this.name);
+    }
+
+}
